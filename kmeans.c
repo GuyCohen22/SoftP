62d75234491b9f58d090e914a78b9a055237696b
Guy Cohen <guyouc@gmail.com>
2025-05-31 08:58:32 +0000

    WIP C file

--- kmeans.c
+++ kmeans.c
@@ -1,7 +1,69 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 const double EPSILON = 0.001;
 const int DEFAULT_ITER = 400; 
+
+typedef struct coordinate {
+    double value;
+    struct coordinate *next;
+} Coordinate;
+
+typedef struct vector
+{
+    struct vector *next;
+    struct coordinate *coordinates;
+} Vector;
+
+/* Returns 1 if a string is a natural number and 0 otherwise. accepts 81.0 as a natural number according to forum clarification */
+int is_natural_number(const char *str) {
+    int i = 0;
+    int dot_seen = 0;
+
+    if (str[0] == '\0') return 0;
+    if (str[0] == '.' && str[1] == '\0') return 0;
+    if (str[0] != '.' && (str[0] < '0' || str[0] > '9')) return 0;
+
+    while (str[i] != '\0') {
+        if (str[i] == '.') {
+            if (dot_seen) return 0;
+            dot_seen = 1;
+        }
+
+        else if (str[i] < '0' || str[i] > '9') {
+            return 0;
+        }
+
+        else if (dot_seen && str[i] != '0') {
+            return 0;
+        }
+        i++;
+    }
+
+    return i >= 1;
+}
+
+
+
+
+/*  Returns the Euclidean distance between two vectors */
+double calculate_euclidean_distance(Vector *v1, Vector *v2) {
+    double sum = 0.0, diff;
+    Coordinate *c1 = v1->coordinates;
+    Coordinate *c2 = v2->coordinates;
+    
+    while (c1 != NULL && c2 != NULL) {
+        diff = c1->value - c2->value;
+        sum += diff * diff;
+        c1 = c1->next;
+        c2 = c2->next;
+    }
+    
+    return sqrt(sum);
+}
+
+int main(int argc, char **argv) {
+    return 0;
+}
